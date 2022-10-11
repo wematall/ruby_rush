bab25b0ff6a2940a793a1583993b973c0d1aa68e
plex <plex@onion.localdomain>
2022-10-11 18:28:50 +0300

    if else variables

---
+++ Lesson_09/if_else_variables.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+hello = "Привет, пацаны!"
+
+hello2 = nil
+
+hello2 = "Привет, девченки!"
+
+hello3 = hello 
+
+devchenki = nil
+
+devchenki = false
+
+if (devchenki == true)
+	puts hello2
+else
+	puts hello
+end
+
+puts "До свидания!"
