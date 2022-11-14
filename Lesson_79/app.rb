1b0e7963b3f67fe849180da95eb08d3fbd1b081b
plex <plex@onion.localdomain>
2022-11-14 17:59:14 +0300

    кубики v.2

--- Lesson_79/app.rb
+++ Lesson_79/app.rb
@@ -2,5 +2,12 @@
 # encoding: utf-8
 
 # Программа, выбрасывающая значение от 1 до 6 — одну игральную кость
+# Cпрашиваем у юзера сколько кубиков кинуть.
 
-puts rand(6) + 1
+puts "How many dice?"
+number = gets.to_i
+
+number.times do 
+	puts rand(6) + 1
+end
+
